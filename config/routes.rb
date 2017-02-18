5960491d9b4062fddcec18644bb575f25c19e4a6
Peter Fitzgibbons <peter.fitzgibbons@gmail.com>
2017-02-18 10:25:35 -0800

    User and User Authentication w/ Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'forums#index'
+
   resources :forums
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
